364c49ac3c78ede744515ae6fc616b0999fcb57c
Lorena Costa <lorenacostace@gmail.com>
2021-06-14 11:50:24 +0200

    feat(Product.js) create Product-organisms

---
+++ src/components/organisms/Product.js
@@ -0,0 +1,23 @@
+import ImageProduct from "../atoms/ImageProduct";
+import DescriptionItem from "../molecules/DescriptionItem";
+import styled from "styled-components";
+
+function Product(props) {
+    const {
+        imgUrl, id, ...description
+    } = props.data;
+    return (
+        <div>
+            <ImageProduct image={imgUrl}/>
+            <SeparationLine/>
+            <DescriptionItem description={description}/>
+        </div>
+    )
+}
+
+const SeparationLine = styled.div`
+  background-color: #E4E7ED;
+  height: 1px;
+`
+
+export default Product
