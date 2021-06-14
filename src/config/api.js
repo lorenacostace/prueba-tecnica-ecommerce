07e083f767276d0852b9b3854cae42db75cbaddf
Lorena Costa <lorenacostace@gmail.com>
2021-06-14 22:06:56 +0200

    feat(api.js, index.js)  added axios configuration and axios cache.

---
+++ src/config/api.js
@@ -0,0 +1,8 @@
+export const configApi = {
+    baseUrl: 'https://003c7590-de68-437f-ba4e-165c1e71c532.mock.pstmn.io',
+    apiVersion: '',
+    endpoint: {
+        product: '/product',
+        cart: '/cart'
+    }
+}
---
+++ src/repository/index.js
@@ -0,0 +1,46 @@
+import { setup } from 'axios-cache-adapter'
+import localforage from 'localforage'
+import memoryDriver from 'localforage-memoryStorageDriver'
+import { configApi } from '../config/api'
+import {
+    getProductById,
+    getProducts
+} from './product'
+import { postCart } from "./cart";
+
+const {
+    endpoint,
+    baseUrl,
+    apiVersion
+} = configApi;
+
+async function configure () {
+    await localforage.defineDriver(memoryDriver)
+    const forageStore = localforage.createInstance({
+        driver: [
+            localforage.INDEXEDDB,
+            localforage.LOCALSTORAGE,
+            memoryDriver._driver
+        ],
+        name: 'cache'
+    })
+    return setup({
+        baseURL: baseUrl + apiVersion,
+        timeout: 5000,
+        cache: {
+            maxAge: 15 * 60 * 1000,
+            store: forageStore,
+            exclude: { query: false },
+            key: req => req.url + JSON.stringify(req.params),
+        }
+    })
+}
+
+export const productRepository = {
+    getProductById: (id) => configure().then(api => getProductById({ api, endpoint: endpoint.product, id})),
+    getProducts: () => configure().then(api => getProducts({ api, endpoint: endpoint.product }))
+}
+
+export const cartRepository = {
+    postCart: (body) => configure().then(api => postCart({ api, endpoint: endpoint.cart, body}))
+}
