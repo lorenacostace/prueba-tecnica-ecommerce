a43052bba62d0839e57cfb4b0d56feb6e48d1d47
Lorena Costa <lorenacostace@gmail.com>
2021-06-15 21:00:00 +0200

    feat(api.js) change of parameters in the Api configuration

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export const configApi = {
-    baseUrl: 'https://003c7590-de68-437f-ba4e-165c1e71c532.mock.pstmn.io',
-    apiVersion: '',
+    baseUrl: process.env.NODE_ENV === 'production' ? 'https://front-test-api.herokuapp.com/' : 'https://003c7590-de68-437f-ba4e-165c1e71c532.mock.pstmn.io',
+    apiVersion: process.env.NODE_ENV === 'production' ? 'api/' : '',
     endpoint: {
         product: '/product',
         cart: '/cart'
