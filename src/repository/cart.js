7d1a8786063031584fc281a103116b794f984786
Lorena Costa <lorenacostace@gmail.com>
2021-06-16 16:50:40 +0200

    fix(cart.js) fix value of storageCode

--- src/repository/cart.js
+++ src/repository/cart.js
@@ -2,6 +2,6 @@ export async function postCart({ api, endpoint, body }) {
     return api.post(endpoint, {
         id: body.id,
         colorCode: body.selectors.colors,
-        storageCode: body.selectors.storage,
+        storageCode: body.selectors.storages,
     }).then(result => result.data);
 }
