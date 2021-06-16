3a5d2a1d14d753e54168c3e863b82b52d55c9e0c
Lorena Costa <lorenacostace@gmail.com>
2021-06-16 16:41:55 +0200

    fix(api.js) fix configuration api

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,9 @@
+/*
+* Se ha preparado la configuración de la Api para el caso en el que tengan distintos dominios resulte sencillo el cambio
+* */
 export const configApi = {
-    baseUrl: process.env.NODE_ENV === 'production' ? 'https://front-test-api.herokuapp.com/' : 'https://003c7590-de68-437f-ba4e-165c1e71c532.mock.pstmn.io',
-    apiVersion: process.env.NODE_ENV === 'production' ? 'api/' : '',
+    baseUrl: process.env.NODE_ENV === 'production' ? 'https://front-test-api.herokuapp.com/' : 'https://front-test-api.herokuapp.com/',
+    apiVersion: process.env.NODE_ENV === 'production' ? 'api/' : 'api/',
     endpoint: {
         product: '/product',
         cart: '/cart'
