340d4fc02c867fc895731e71189b440c71d0ee55
lorenacostace <lorenacostace@gmail.com>
2021-07-06 11:24:35 +0200

    fix(service.js) fix special regex symbols

--- src/helpers/service.js
+++ src/helpers/service.js
@@ -1,7 +1,12 @@
 
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
 export const updateFilter = (products, value) => {
+    const valueRegex = escapeRegExp(value)
     return products.filter((item) => {
-        const regex = new RegExp(value, 'i')
+        const regex = new RegExp(valueRegex, 'i')
         return (item.brand.match(regex) || item.model.match(regex));
     })
 }
